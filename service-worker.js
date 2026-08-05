665817393eb3cbd98c4684c3c350ed39812eae1d
dadididou429-glitch <dadididou429@gmail.com>
2026-08-05 04:04:59 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Pharis Service Worker — v9 (fast update)
 // غيّر رقم الإصدار عند كل تحديث ليفرض التحديث فورًا
-const CACHE_NAME = "pharis-v9";
+const CACHE_NAME = "pharis-v10";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 const EXTERNAL = [
