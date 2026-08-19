5cb3ccbda83bc9476f83858b7faa10c286c8dd57
dadididou429-glitch <dadididou429@gmail.com>
2026-08-19 04:30:41 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Pharis Service Worker — fast update
-const CACHE_NAME = "pharis-v36-onboard-always";
+const CACHE_NAME = "pharis-v37-med-search";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 const EXTERNAL = [
