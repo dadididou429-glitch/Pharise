9dd235de210981c9a57044eb3f90c68664855d0e
dadididou429-glitch <dadididou429@gmail.com>
2026-08-20 04:37:52 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Pharis Service Worker — fast update ≤5s
-const CACHE_NAME = "pharis-v53-update-btn";
+const CACHE_NAME = "pharis-v54-intro-music";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./splash-intro.mp4"];
 
 const EXTERNAL = [
