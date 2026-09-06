d385a4f6799f9104ce8c6faa4542a3aa00f49ede
dadididou429-glitch <dadididou429@gmail.com>
2026-09-06 23:30:11 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Pharis Service Worker — تحديث فوري + أوفلاين
-const CACHE_NAME = "pharis-v124-sba-final";
+const CACHE_NAME = "pharis-v127-coverage-fix";
 const SHELL = [
   "./",
   "./index.html",
